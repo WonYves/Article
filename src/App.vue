a54ae9b0e9e530eb5d70ebe666514222542e74a2
WonYves <1053137796@qq.com>
2022-10-10 13:52:51 +0800

    初次路由

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <el-button>默认按钮</el-button>
+  <div>
+     <!-- 一级路由规则对象组件的挂载点 -->
+    <!-- <router-view></router-view> -->
+  </div>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -7,8 +7,14 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/login',
-    name: 'login',
-    component: () => import('@/views/Login.vue')
+    component: () => import('@/views/login')  
+    // webpack提供的让import函数来路由懒加载导入组件
+    // 路由懒加载 当切换到login时  才加载对应组件的代码 
+    // 好处是为了让首页体积更小 打开的时候更快
+  },
+  {
+    path: '/reg',
+    component: () => import('@/views/register')
   }
 ]
 
